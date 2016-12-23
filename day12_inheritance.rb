5b28fb3f8d0e1e6b9c1712dfef3a01d4a1384877
toowan <mail@toowan.me>
2016-12-23 01:49:38 -0800

    reformatted my solution

--- day12_inheritance.rb
+++ day12_inheritance.rb
@@ -1,42 +1,45 @@
+### Provided by Hacker Rank
 class Person
   # instance variables
-	def initialize(firstName, lastName, id)
-		@firstName = firstName
-		@lastName = lastName
-		@id = id
-	end
+  def initialize(firstName, lastName, id)
+    @firstName = firstName
+    @lastName = lastName
+    @id = id
+  end
 
   # Print info about an instance of Person
-	def printPerson()
-		print("Name: ",@lastName , ", " + @firstName ,"\nID: " , @id)
-	end
+  def printPerson()
+    print("Name: ",@lastName , ", " + @firstName ,"\nID: " , @id)
+  end
 end
 
+### Only this block below is my code
 class Student < Person
   def initialize(firstName, lastName, id, scores)
-      super(firstName, lastName, id)
-      @scores = (scores.inject(:+))/numScores
+    super(firstName, lastName, id)
+    @scores = (scores.inject(:+))/numScores
   end
 
   def calculate()
-      if @scores <= 100 && @scores >= 90
-        letter_grade = "O"
-      elsif @scores < 90 && @scores  >= 80
-        letter_grade = "E"
-      elsif @scores < 80 && @scores >= 70
-        letter_grade = "A"
-      elsif @scores < 70 && @scores >= 55
-        letter_grade = "P"
-      elsif @scores < 55 && @scores >= 40
-        letter_grade = "D"
-      else @scores < 40
-        letter_grade = "T"
+    if @scores <= 100 && @scores >= 90
+      letter_grade = "O"
+    elsif @scores < 90 && @scores  >= 80
+      letter_grade = "E"
+    elsif @scores < 80 && @scores >= 70
+      letter_grade = "A"
+    elsif @scores < 70 && @scores >= 55
+      letter_grade = "P"
+    elsif @scores < 55 && @scores >= 40
+      letter_grade = "D"
+    else @scores < 40
+      letter_grade = "T"
     end
 
     return letter_grade
   end
 end
 
+### Below code is provided by Hacker Rank
 
 input = gets.split()
 firstName = input[0]
