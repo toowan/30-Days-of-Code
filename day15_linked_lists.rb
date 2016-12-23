f2d61e73492a42159ff659cc20e124b42d89729a
toowan <mail@toowan.me>
2016-12-23 15:23:57 -0800

    Day 15: Linked List in Ruby

--- day15_linked_lists.rb
+++ day15_linked_lists.rb
@@ -1,26 +1,37 @@
+## Provided by Hacker Rank
 class Node
-
-    attr_accessor :data,:next
-
+  attr_accessor :data,:next
   def initialize data
-        @data = data
-        @next = nil
-    end
+    @data = data
+    @next = nil
+  end
 end
 
 class Solution
-def insert(head,value)
+  # Complete the insert function so that it creates a new Node (pass data as the Node constructor
+  # argument) and inserts it at the tail of the linked list referenced by the head parameter. Once the
+  # new node is added, return the head reference to the node.
+  def insert(head, data)
+      if (head == nil)  # if there's no head create a new node and set it as the head
+          head = Node.new(data)
+      else
+          current = head  # set current node as the head
+          while current.next != nil # while the next one is not the tail, set the current node to the next node.
+              current = current.next
+          end
+          current.next = Node.new(data) # create a new node to insert into the sequence at tail
+      end
+      return head  # return head reference to the node
+  end
 
-
-end
-
-def display(head)
-     current = head
-     while current
-        print current.data," "
-        current = current.next
-     end
- end
+## Provided by Hacker Rank
+  def display(head)
+    current = head
+    while current
+      print current.data," "
+      current = current.next
+    end
+  end
 end
 
 mylist= Solution.new
