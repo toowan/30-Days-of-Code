c387b69a44226d17b78fe496128e9d5a5b6b0cca
toowan <mail@toowan.me>
2017-01-03 11:39:57 -0800

    Day 27: Testing in Ruby

---
+++ day27_testing.rb
@@ -0,0 +1,30 @@
+## TEST CASES ##
+
+# t      => # of lectures
+# n  k   => # of students, # of students that must be present 
+# A      => # students' arrival times
+
+
+# PRINT 11 LINES OF OUTPUT THAT CAN BE READ BY THE CHALLENGE AS VALID INPUT, AND THE 
+# CHALLENGE SHOULD READ SAID INPUT AND OUTPUT THE FOLLOWING: 
+
+# YES
+# NO
+# YES
+# NO
+# YES
+
+
+puts('5')
+puts('4 3')
+puts('-1 0 4 2')
+puts('6 1')
+puts('-2 -4 -5 0 3 4')
+puts('5 3')
+puts('0 -1 5 6 7')
+puts('3 2')
+puts('-9 0 10')
+puts('7 6')
+puts('-9 -8 -7 0 9 10 11')
+
+
